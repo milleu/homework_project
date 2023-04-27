e200716d1c04d13426bdd34d763dd422f1b0b6ed
Daria Beliaeva <dariabeliaeva22@gmail.com>
2023-04-27 16:35:21 -0700

    function 2 was added

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def get_letters(word):
+    ''' возвращает слово заглавными буквами'''
     return word.upper()
+
+def get_first_letter(word):
+    ''' возвращает  первую букву заглавной'''
+    return word.title()
